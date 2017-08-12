daf383219a3413ce22a3268a79ca4faa89e687b6
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-12 14:12:05 -0300

    Fixing tests

--- tests/src/app/index.test.js
+++ tests/src/app/index.test.js
@@ -1,12 +1,12 @@
 import 'react-native'
 import React from 'react'
-import Index from '../index.android.js'
+import App from '../../../src/app'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
   renderer.create(
-    <Index />
+    <App />
   )
 })
