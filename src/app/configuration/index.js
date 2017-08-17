b0b43cafc2e87ddb1b2a430591cedc12369eae76
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-17 15:43:04 -0300

    Add nachos-ui

--- src/app/configuration/index.js
+++ src/app/configuration/index.js
@@ -4,10 +4,12 @@ import { initLocales } from './i18n'
 import { registerScreens } from './screens'
 import { startApp } from './navigation'
 import { initializeFirebase } from './firebase'
+import { setNachosThemes } from './themes'
 
 export default {
   initLocales,
   registerScreens,
   startApp,
-  initializeFirebase
+  initializeFirebase,
+  setNachosThemes
 }
