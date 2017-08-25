360c43b71b441e7e3c17e22585d79d1d5849500c
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-25 11:55:12 -0300

    Changing user to class

--- src/services/firebase/authentication.js
+++ src/services/firebase/authentication.js
@@ -1,20 +1,10 @@
 // @flow
 
+import { User } from './user'
+
 let auth
 
-type User = {
-  email: string,
-  emailVerified: boolean,
-  uid: string,
-  providerData: Array<string>
-}
-
-function mapFirebaseUser (firebaseUser): User {
-  const { email, emailVerified, uid, providerData } = firebaseUser
-  return { email, emailVerified, uid, providerData }
-}
-
-// TODO: react-native-firebase, has types declaration. Figure out how to import it.
+// TODO: react-native-firebase, has types declaration. Figure out how to import it. https://flow.org/en/docs/types/modules/
 export function initializeAuth (authInstance: Object) {
   auth = authInstance
 }
@@ -22,7 +12,7 @@ export function initializeAuth (authInstance: Object) {
 export async function createUserWithEmailAndPassword (email: string, password: string) {
   try {
     const firebaseUser = await auth.createUserWithEmailAndPassword(email, password)
-    return mapFirebaseUser(firebaseUser)
+    return new User(firebaseUser)
   } catch (error) {
     console.error('An error occurred', error)
   }
@@ -31,28 +21,24 @@ export async function createUserWithEmailAndPassword (email: string, password: s
 export async function signInWithEmailAndPassword (email: string, password: string) {
   try {
     const firebaseUser = await auth.signInWithEmailAndPassword(email, password)
-    return mapFirebaseUser(firebaseUser)
+    return new User(firebaseUser)
   } catch (error) {
     console.error('signin error', error)
   }
 }
 
-export function sendPasswordResetEmail (email: string) {
-  return auth.sendPasswordResetEmail(email)
-    .then(() => {
-      console.log('Password reset email sent')
-    })
-    .catch((error) => {
-      console.error('Unable send password reset email', error)
-    })
+export async function sendPasswordResetEmail (email: string) {
+  try {
+    await auth.sendPasswordResetEmail(email)
+  } catch (error) {
+    console.error('Unable send password reset email', error)
+  }
 }
 
-export function signOut () {
-  return auth.signOut()
-    .then(() => {
-      console.log('mapFirebaseUser signed out successfully')
-    })
-    .catch((error) => {
-      console.error('Error during sign out', error)
-    })
+export async function signOut () {
+  try {
+    await auth.signOut()
+  } catch (error) {
+    console.error('Error during sign out', error)
+  }
 }
