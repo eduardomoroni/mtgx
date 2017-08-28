d824cb57f527ce393ec36523c1a7fefa72dd3277
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-28 18:53:43 -0300

    react-native link realm

--- android/app/src/main/java/com/mtgx/MainApplication.java
+++ android/app/src/main/java/com/mtgx/MainApplication.java
@@ -23,6 +23,7 @@ public class MainApplication extends NavigationApplication {
   protected List<ReactPackage> getPackages() {
     return Arrays.<ReactPackage>asList(
         new MainReactPackage(),
+            new RealmReactPackage(),
         new ReactNativeI18n(),
         new ReactNativeConfigPackage(),
         new RNFirebasePackage(),
