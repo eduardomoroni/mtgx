63b2d1baefdcfc7be6c1fe7f2c59e336960c072d
Eduardo Moroni <eduardomoroni@gmail.com>
2017-08-31 08:15:26 -0300

    feat: add redux-form

--- src/modules/cardSearch/components/cardSearchScreen.js
+++ src/modules/cardSearch/components/cardSearchScreen.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { View } from 'react-native'
 import { ManaSymbolBar } from './manaSymbolBar'
+import { Field } from 'redux-form/immutable'
 
 import { styles } from './styles/cardSearch.styles'
 
 export class CardSearchScreen extends Component {
+
+  static propTypes = {
+    colors: PropTypes.arrayOf(PropTypes.string)
+  }
+
   render () {
     return (
       <View style={styles.container}>
-        <ManaSymbolBar input={{onChange: (value) => console.log(value), value: 'TESTE'}} />
+        <Field name='cardColors' component={ManaSymbolBar} />
       </View>
     )
   }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,14 @@
 import { Record } from 'immutable'
 import { combineReducers } from 'redux-immutable'
+
 import AuthenticationReducer, { initialState as authInitialState } from './authenticationReducer'
 import MessageReducer, { initialState as messageInitialState } from './messageReducer'
+import { reducer as ReduxForm } from 'redux-form/immutable'
 
 const reducers = {
   authentication: AuthenticationReducer,
-  message: MessageReducer
+  message: MessageReducer,
+  form: ReduxForm
 }
 
 export const initialState = {
