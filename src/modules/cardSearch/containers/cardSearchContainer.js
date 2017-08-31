aed9460ec385a3c5d2d7dd57b16364aabf4c8a99
Eduardo Moroni <eduardomoronI@gmail.com>
2017-08-31 08:31:25 -0300

    add redux form (#33)
    * feat: add redux-form
    
    * feat: add redux-form
    
    * fix: update flowtyped libdefs
    
    * fix(flow-typed): created redux-form fake libdef
    
    npm run flow-install-libdefs wasn't generating redux-form libdef, so I've created for my own
    
    * ci(package.json): change precommit hok
    
    running flow during precommit hook was taking to long

--- src/modules/cardSearch/containers/cardSearchContainer.js
+++ src/modules/cardSearch/containers/cardSearchContainer.js
@@ -3,22 +3,33 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import type { Dispatch } from 'redux'
+import { reduxForm, formValueSelector } from 'redux-form/immutable'
+
 import { CardSearchScreen } from '../components/cardSearchScreen'
 
 class CardSearchScreenContainer extends Component {
   render () {
     return (
-      <CardSearchScreen />
+      <CardSearchScreen
+        color={this.props.colors}
+      />
     )
   }
 }
 
+const form = 'CardSearch'
+
 const mapStateToProps = (state) => {
-  return {}
+  const selector = formValueSelector(form)
+
+  return {
+    colors: selector(state, 'colors')
+  }
 }
 
 const mapDispatchToProps = (dispatch: Dispatch<*>) => {
   return {}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(CardSearchScreenContainer)
+const cardSearchContainerDecorated = reduxForm({ form })(CardSearchScreenContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(cardSearchContainerDecorated)
