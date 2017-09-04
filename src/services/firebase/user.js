b054324499b8e0f699a972738ce956d8ef70aa4a
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-04 18:26:46 -0300

    ci: fix services flow

--- src/services/firebase/user.js
+++ src/services/firebase/user.js
@@ -1,10 +1,12 @@
+// @flow
+
 export class User {
   uid: string
   email: string
   emailVerified: boolean
   providerData: Array<string>
 
-  constructor (firebaseUser) {
+  constructor (firebaseUser: Object) {
     const { email, emailVerified, uid, providerData } = firebaseUser
     Object.assign(this, { email, emailVerified, uid, providerData })
   }
