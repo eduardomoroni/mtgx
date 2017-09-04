06d8015f935ae51496e6d28284fac9cba355def2
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-04 17:38:35 -0300

    ci(cardsThunk): fix flow

--- src/redux/thunks/cardsThunks.js
+++ src/redux/thunks/cardsThunks.js
@@ -3,9 +3,11 @@
 import { warn } from '../../services/logger'
 import { queryByForm } from '../../services/realm/cardService'
 import { setCards } from '../actions/cardsActions'
+import type { Map } from 'immutable'
+import type { Dispatch } from 'redux'
 
-export function queryCardByForm (form) {
-  return async (dispatch) => {
+export function queryCardByForm (form: Map) {
+  return async (dispatch: Dispatch<*>) => {
     try {
       const cards = await queryByForm(form)
       console.log('===> cards is: ', cards)
