94b24ea5be29a103f0a4fd894d63017245f7a9da
Eduardo Moroni <eduardomoroni@gmail.com>
2017-09-13 11:07:27 -0300

    feat: add MTG JSON set import function

---
+++ src/assets/cards/index.js
@@ -0,0 +1,19 @@
+import AER from './AER-x.json'
+import AKH from './AKH-x.json'
+import EMN from './EMN-x.json'
+import HOU from './HOU-x.json'
+import KLD from './KLD-x.json'
+import SOI from './SOI-x.json'
+import W17 from './W17-x.json'
+import W16 from './W16-x.json'
+
+export const sets = {
+  AER,
+  AKH,
+  EMN,
+  HOU,
+  KLD,
+  SOI,
+  W16,
+  W17
+}
